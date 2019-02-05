07194b9d2dc47a5de76114ac05ae0499304ad331
jcraggs <46655916+jcraggs@users.noreply.github.com>
2019-02-05 22:29:08 +0000

    cs50_pset2_caesar_v2.py
    Refactored code to python standards.

--- cs50_pset2_caesar.py
+++ cs50_pset2_caesar.py
@@ -1,91 +1,96 @@
-#----FIRST STAGE: Getting user inputs
-word = input("> Enter the message you want encrypted: ")
-entered_word = list(str(word)) #converts message to a list
-password_input = input("> Enter a password (case sensitive) for your message: ")
-
-#----Used to set the scrambling of the message and catch any user errors
-globvar = 0
-def set_globvar_to_one():
-		global globvar
-		globvar += 1
-
-while globvar == 0:
-	scrambler = input("> Enter a number between 1 and 26 to scramble your message: ") #Used to set the scramble offset (Noe this value will be converted to -1 to -26, as outside this range the asciii does not output well)
-	try:
-		while int(scrambler) > 26 or int(scrambler) < 1:
-			print("Error_1: Number must be between 1 and 26 \n")
-			scrambler = input("> Enter a number between 1 and 26 to scramble your message: ")
-		if 1 <= int(scrambler) <= 26:
-			pass
-			set_globvar_to_one()
-	except ValueError:
-		print("Error_2: Input must be a number between 1 and 26 \n")
-
-int_scrambler = int(scrambler)*-1 # used to convert the user input from postive to negative intergers
+"""A program which ciphers a user input message by shifting unicode characters"""
+def main():
+    """Main program"""
+    clear_text, int_scrambler, password_input = user_inputs()
+    rev_cipher_msg = cipher_message(clear_text, int_scrambler)
+    password_test(password_input)
+    decipher_message(rev_cipher_msg, int_scrambler)
 
 
-#---- SECOND STAGE: Ciphering the user message
+def user_inputs():#FIRST STAGE: Getting user inputs
+    """This function gets the user inputs"""
+    clear_text = input("> Enter the message you want encrypted: ")
+    password_input = input("> Enter a password (case sensitive) for your message: ")
 
-#Converting the entered_word list to ascii data array#
-ord_input = []
-for item in entered_word:
-	ord_input.append(ord(item))
+    #used to set scramble amount for the message and catch any user errors
+    while True:
+        scrambler = input("> Enter a number between 1 and 26 to scramble your message: ")
+        try:
+            if 1 <= int(scrambler) <= 26:
+                break
+            else:
+                print("Error_1: Number must be between 1 and 26 \n")
+        except ValueError:
+            print("Error_2: Input must be a number between 1 and 26 \n")
 
-#Applying the scrambler variable to the unicode numbers to cipher the data
-cipher_data = []
-for item in ord_input:
-	cipher_data.append(item + int_scrambler)
+    # used to convert the user input from postive to negative intergers
+    int_scrambler = int(scrambler)*-1
 
-#Printing the scrambled message
-cipher_msg = []
-for item in cipher_data:
-	cipher_msg.append(chr(item))
-rev_cipher_msg = cipher_msg[::-1] #this reverses the cipher message to add an extra level of scrambling
+    return clear_text, int_scrambler, password_input
 
-print("\nYour ciphered message is displayed below: \n")
-for item in rev_cipher_msg:
-    print(item, end="")
-print()
 
-print("\n-------------------------------------------------------")
+def cipher_message(clear_text, int_scrambler):
+    """This function ciphers the users message"""
+    clear_text_list = list(str(clear_text)) #converts clear_text message to a list
+    ord_input = []
 
-#---- THIRD STAGE: Asking the user for a password to unlock the message
-t_f_val = 0
-def pass_check():
-	t_f_val = 0
-	x = 3
-	def set_t_f_val_to_1():
-		global t_f_val
-		t_f_val += 1
+    for item in clear_text_list:
+        ord_input.append(ord(item))
 
-	password_check = input("\n> Enter the password (case sensitive) to access this message: ")
-	while password_input != password_check and x >= 1:
-			print("Password is incorrect: " + str(x) + " more retry(s) left \n")
-			x -= 1
-			password_check = input("> Enter the password (case sensitive) to access this message: ")
+    #Applying the scrambler variable to the unicode numbers to cipher the data
+    cipher_data = []
+    for item in ord_input:
+        cipher_data.append(item + int_scrambler)
 
-	if password_input == password_check:
-			pass
-			print("Correct password entered, here is the deciphered message: \n")
-			set_t_f_val_to_1()
+    #Printing the scrambled message
+    cipher_msg = []
+    for item in cipher_data:
+        cipher_msg.append(chr(item))
 
-	if x == 0:
-		print("\nERROR: Too many password attempts made, exiting program.")
-		exit()
-pass_check()
+    #this reverses the cipher message to add an extra level of scrambling
+    rev_cipher_msg = cipher_msg[::-1]
 
-#---- FOURTH STAGE: Deciphering and displaying the user the message (if password has been correctly entered)
+    print("\nYour ciphered message is displayed below: \n")
+    for item in rev_cipher_msg:
+        print(item, end="")
+    print()
+    print("\n-------------------------------------------------------")
 
-#Converting the ciphered message to unicode and undo-ing the scambling function + reversal
-decipher_data = []
-for item in rev_cipher_msg:
-	decipher_data.append(ord(item) - int_scrambler)
-rerev_decipher_data = decipher_data[::-1]#reverses the data back to normal
+    return rev_cipher_msg
 
-#converts re-reversed deciphered data from unicode to ascii characters
-chr_decipher = []
-for item in rerev_decipher_data:
-	chr_decipher.append(chr(item))
 
-for item in chr_decipher:
-    print(item, end="")
+def password_test(password_input):
+    """This function asks the user for a password to unlock the message"""
+    attempts = 3 #allows a max of 3 failed attempts
+    while True:
+        password_check = input("\n> Enter the password (case sensitive) to access this message: ")
+        if password_input != password_check and attempts >= 0:
+            print("Password is incorrect: " + str(attempts) + " more retry(s) left \n")
+            attempts -= 1
+        if password_input == password_check:
+            break
+        if attempts == 0:
+            print("\nERROR: Too many password attempts made, exiting program.")
+            exit()
+
+
+def decipher_message(rev_cipher_msg, int_scrambler):
+    """This function diciphers and then displays the user message"""
+    print("\nYour deciphered message is displayed below: \n")
+    decipher_data = []
+    for item in rev_cipher_msg:
+        decipher_data.append(ord(item) - int_scrambler) #re-aligns to original unicode
+    rerev_decipher_data = decipher_data[::-1]#re-reverses the message back to orignal
+
+    #converts re-reversed deciphered data from unicode to ascii characters
+    chr_decipher = []
+    for item in rerev_decipher_data:
+        chr_decipher.append(chr(item))
+
+    #prints the deciphered message
+    for item in chr_decipher:
+        print(item, end="")
+
+
+if __name__ == "__main__":
+    main()
