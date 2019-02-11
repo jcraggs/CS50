974cc24efc6028b41a3169ef237e32841e4a245e
jcraggs <46655916+jcraggs@users.noreply.github.com>
2019-02-11 20:40:11 +0000

    Update cs50_pset1_mario.py

--- cs50_pset1_mario.py
+++ cs50_pset1_mario.py
@@ -1,33 +1,39 @@
-globvar = 0
+"""A program which builds a staircase made of #'s with the size dependent on user input"""
+def main():
+    """Main program"""
+    val = user_inputs()
+    print_staircase(val)
 
-def mario_world_builder():
-	user_input = input("How tall do you want Mario's Pyramid? (Enter a number between 1 - 8): ") 
-	rolling_number = 0
-	rolling_reverse = 0
-	space_var = " "
-	hash_var = "#"
 
-	def set_globvar_to_one():
-		global globvar
-		globvar += 1
+def user_inputs():
+    """This function gets the user inputs"""
+    while True:
+        user_input = input(
+            "How tall do you want Mario's staircase? (Enter a number between 1 - 8): ")
+        try:
+            val = int(user_input)
+            if val > 8 or val < 1:
+                print("Error_1: Number must be between 1-8 \n")
+            if 1 <= val <= 9:
+                break
+        except ValueError:
+            print("Error_2: Input must be a number between 1-8 \n")
+    return val
 
-	try:
-		val = int(user_input)
-		while val > 8 or val < 1:
-			print("Error_1: Number must be between 1-8 \n")
-			user_input = input("How tall do you want Mario's Pyramid? (Enter a number between 1 - 8): ")
-			val = int(user_input)
-		if 1 <= val <= 9:
-			pass
-			set_globvar_to_one()
-			rolling_reverse= int(user_input)
-			while rolling_number < val+1:
-				print(" "+(rolling_reverse)*space_var + (1*hash_var)*rolling_number)
-				rolling_number += 1
-				rolling_reverse -= 1
-			print()
-	except ValueError:
-		print("Error_2: Input must be a number between 1-8 \n")
 
-while globvar ==0:
-	mario_world_builder()
+def print_staircase(val):
+    """This function prints the staircase of #'s"""
+    rolling_number = 0
+    rolling_reverse = val
+    space_var = " "
+    hash_var = "#"
+
+    while rolling_number < val+1:
+        print(" "+(rolling_reverse)*space_var + (1*hash_var)*rolling_number)
+        rolling_number += 1
+        rolling_reverse -= 1
+    print()
+
+
+if __name__ == "__main__":
+    main()
