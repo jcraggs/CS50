fc0def9ee9b70920f9d0327c272ab926b0385f6c
jcraggs <jonathancraggs@hotmail.co.uk>
2019-02-18 19:19:25 +0000

    Refactored

--- cs50_pset1_change.py
+++ cs50_pset1_change.py
@@ -1,18 +1,27 @@
-"""A program which calculates the change for a purchase"""
+"""A program which calculates the change for a purchase dependent on user input"""
 def main():
     """Main program"""
-    cost = cost_input()
-    paid = paid_input(cost)
-    rounded_change_val = change_req(cost, paid)
-    smallest_change(rounded_change_val)
+    denominations, local_crncy = currency_denom()
+    cost = cost_input(local_crncy)
+    paid = paid_input(cost, local_crncy)
+    rcv = change_req(cost, paid, local_crncy)
+    smallest_change(rcv, local_crncy, denominations)
+
+def currency_denom():
+    """Allows code to be edited to suit different currency denominations"""
+    denominations = sorted([50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]) #edit this to change denominations
+    local_crncy = "£" #edit this depending on currency
+
+    return denominations, local_crncy
 
 
-def cost_input():
+
+def cost_input(local_crncy):
     """Getting the user to input the cost of the item"""
     while True:
         try:
             cost = float(input(
-                "How much does the item cost (enter value as a number with decimals i.e 3.99 for £3.99)?: "))
+                "How much does the item cost (enter value as a number with decimals i.e 3.99 for "+ local_crncy +"3.99)?: "))
             if cost < 0 or cost == 0:
                 print("Error_1: Cost must be entered as a number greater than 0 \n")
             if cost > 0:
@@ -23,12 +32,12 @@ def cost_input():
     return cost
 
 
-def paid_input(cost):
+def paid_input(cost, local_crncy):
     """Getting the user to input how much the customer has paid for the item"""
     while True:
         try:
             paid = float(input(
-                "How much has the customer paid (enter value as a number with decimals i.e 3.99 for £3.99)?: "))
+                "How much has the customer paid (enter value as a number with decimals i.e 3.99 for "+ local_crncy+ "3.99)?: "))
             if paid < cost:
                 print("Error_1: Payment must be greater than the cost of the item \n")
             if paid > cost or paid == cost:
@@ -39,121 +48,32 @@ def paid_input(cost):
     return paid
 
 
-def change_req(cost, paid):
+def change_req(cost, paid, local_crncy):
     """Displaying the amount of change the customer requires"""
     change_val = (paid - cost)
-    rounded_change_val = round(change_val, 2)
-    if rounded_change_val == 0:
+    rcv = round(change_val, 2)
+    if rcv == 0:
         print("\nThe customer does not require change.")
         exit()
-    if rounded_change_val > 0:
-        print("\nThe customer is owed: £" + str(rounded_change_val))
+    if rcv > 0:
+        print("\nThe customer is owed: "+ local_crncy + str(rcv))
 
-    return rounded_change_val
+    return rcv
 
 
-def smallest_change(rounded_change_val):
+def smallest_change(rcv, local_crncy, denominations):
     """Calculating and displaying the fewest denominations required to give the customer their change"""
-    whole_note_50 = 0
-    whole_note_20 = 0
-    whole_note_10 = 0
-    whole_note_5 = 0
-    whole_pound_2 = 0
-    whole_pound_1 = 0
-    whole_pence_50 = 0
-    whole_pence_20 = 0
-    whole_pence_10 = 0
-    whole_pence_5 = 0
-    whole_pence_2 = 0
-    whole_pence_1 = 0
-    rolling_change_val = rounded_change_val
+    print("\nYou need to give the customer the following change denominations: \n")
 
-    while rolling_change_val > 50 or rolling_change_val == 50:
-        note_50 = (rounded_change_val/50)
-        whole_note_50 = int(note_50)
-        rem_change = round(((note_50-whole_note_50)*50), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 20 or rolling_change_val == 20:
-        note_20 = (rolling_change_val/20)
-        whole_note_20 = int(note_20)
-        rem_change = round(((note_20-whole_note_20)*20), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 10 or rolling_change_val == 10:
-        note_10 = (rolling_change_val/10)
-        whole_note_10 = int(note_10)
-        rem_change = round(((note_10-whole_note_10)*10), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 5 or rolling_change_val == 5:
-        note_5 = (rolling_change_val/5)
-        whole_note_5 = int(note_5)
-        rem_change = round(((note_5-whole_note_5)*5), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 2 or rolling_change_val == 2:
-        pound_2 = (rolling_change_val/2)
-        whole_pound_2 = int(pound_2)
-        rem_change = round(((pound_2-whole_pound_2)*2), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 1 or rolling_change_val == 1:
-        pound_1 = (rolling_change_val/1)
-        whole_pound_1 = int(pound_1)
-        rem_change = round(((pound_1-whole_pound_1)*1), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 0.5 or rolling_change_val == 0.5:
-        pence_50 = (rolling_change_val/0.5)
-        whole_pence_50 = int(pence_50)
-        rem_change = round(((pence_50-whole_pence_50)*0.5), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 0.2 or rolling_change_val == 0.2:
-        pence_20 = (rolling_change_val/0.2)
-        whole_pence_20 = int(pence_20)
-        rem_change = round(((pence_20-whole_pence_20)*0.2), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 0.1 or rolling_change_val == 0.1:
-        pence_10 = (rolling_change_val/0.1)
-        whole_pence_10 = int(pence_10)
-        rem_change = round(((pence_10-whole_pence_10)*0.1), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 0.05 or rolling_change_val == 0.05:
-        pence_5 = (rolling_change_val/0.05)
-        whole_pence_5 = int(pence_5)
-        rem_change = round(((pence_5-whole_pence_5)*0.05), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 0.02 or rolling_change_val == 0.02:
-        pence_2 = (rolling_change_val/0.02)
-        whole_pence_2 = int(pence_2)
-        rem_change = round(((pence_2-whole_pence_2)*0.02), 2)
-        rolling_change_val = rem_change
-    while rolling_change_val > 0.01 or rolling_change_val == 0.01:
-        pence_1 = (rolling_change_val/0.01)
-        whole_pence_1 = int(pence_1)
-        rem_change = round(((pence_1-whole_pence_1)*0.01), 2)
-        rolling_change_val = rem_change
+    denominations.reverse()
 
-    print("You need to give the customer the following change denominations: \n")
-    if whole_note_50 > 0:
-        print("- £50 note x " + str(whole_note_50))
-    if whole_note_20 > 0:
-        print("- £20 note x " + str(whole_note_20))
-    if whole_note_10 > 0:
-        print("- £10 note x " + str(whole_note_10))
-    if whole_note_5 > 0:
-        print("- £5 note x " + str(whole_note_5))
-    if whole_pound_2 > 0:
-        print("- £2 coin x " + str(whole_pound_2))
-    if whole_pound_1 > 0:
-        print("- £1 coin x " + str(whole_pound_1))
-    if whole_pence_50 > 0:
-        print("- 50p coin x " + str(whole_pence_50))
-    if whole_pence_20 > 0:
-        print("- 20p coin x " + str(whole_pence_20))
-    if whole_pence_10 > 0:
-        print("- 10p coin x " + str(whole_pence_10))
-    if whole_pence_5 > 0:
-        print("- 5p coin x " + str(whole_pence_5))
-    if whole_pence_2 > 0:
-        print("- 2p coin x " + str(whole_pence_2))
-    if whole_pence_1 > 0:
-        print("- 1p coin x " + str(whole_pence_1))
+    while rcv > 0:
+        for i in denominations:
+            if i <= rcv:
+                divid_change = rcv/i
+                whole_change = int(divid_change)
+                print("- " + local_crncy + str(i) + " x " + str(whole_change))
+                rcv = round(((divid_change-whole_change)*i), 2)
 
 
 if __name__ == "__main__":
